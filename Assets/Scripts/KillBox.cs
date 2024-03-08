80d1082f92db67dc19900531b537ea095b0391f4
sawl-an-dawnen <78926681+sawl-an-dawnen@users.noreply.github.com>
2024-03-07 21:36:21 -0600

    Sound Update
    -added splash
    -added ambient noise to sewer
    -updated camera, light, and fog settings

--- Assets/Scripts/KillBox.cs
+++ Assets/Scripts/KillBox.cs
@@ -8,17 +8,22 @@ public class KillBox : MonoBehaviour
     PlayerManager playerManager;
     SpawnManager spawnManager;
 
+    AudioSource audioSource;
+    public AudioClip deathSound;
+
 
     // Start is called before the first frame update
     void Start()
     {
         playerManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
         spawnManager = GameObject.FindGameObjectWithTag("Player").GetComponent<SpawnManager>();
+        audioSource = GameObject.FindGameObjectWithTag("SFX-2").GetComponent<AudioSource>();
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) {
+            audioSource.PlayOneShot(deathSound);
             playerManager.SubLife();
             spawnManager.Respawn();
         }
